0b7152b99974a9c141c7bfd0f84579e7714eaa35
daren <dd920907@gmail.com>
2025-05-12 00:03:35 +0800

    時間日期顯示
    - 重新修改 main.py 和 hello.py
    - 增加時間日期功能，以及問候語句

--- hello.py
+++ hello.py
@@ -1,2 +1,10 @@
+# 取得目前的日期與時間
+from datetime import datetime
+
+# 定義 hello 函數，接收一個字串參數 name
 def hello(name: str) -> None:
-    print(f"Hello 5/11 {name}")
+    # 取得現在的時間，格式化成字串（年-月-日 時:分:秒）
+    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    
+    # 輸出問候語句和目前時間的訊息
+    print(f"Hello {name}，現在是時間是 {now}")
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from hello import hello
 
 if __name__ == "__main__":
-    name = "你的名字是甚麼"
+    # 定義一個問候語句，向使用者問好
+    name = "你好，你的名字是? \n今天過得好嗎"
+    # 呼叫 hello 函數，將問候語句傳入
     hello(name)
